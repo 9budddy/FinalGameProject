2d50465055385e3368d83edc41bef7d1abc6c58b
Budddy <Budddyjellz@gmail.com>
2023-11-29 01:43:01 -0500

    Pickup Completion of Weapons

--- Assets/Scripts/PlayerWeaponCollider.cs
+++ Assets/Scripts/PlayerWeaponCollider.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerWeaponCollider : MonoBehaviour
 {
     private bool sword, bow;
+    private bool start = false;
 
     private Animator animator;
     [SerializeField] private GameState gameState;
@@ -22,7 +25,7 @@ public class PlayerWeaponCollider : MonoBehaviour
     }
 
     
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Sword")
         {
@@ -40,16 +43,8 @@ public class PlayerWeaponCollider : MonoBehaviour
                 overlayAnimator.SetBool("bow", false);
                 overlayAnimator.SetBool("sword", false);
 
-
-                animator.SetBool("sword", true);
-                weaponAnimator.SetBool("sword", true);
-                overlayAnimator.SetBool("sword", true);
-
-
-                gameState.bow = false;
-                gameState.sword = true;
-                gameState.changeWeapon = true;
-                weaponSR.enabled = true;
+                start = true;
+                StartCoroutine(SetSword());
             }
         }
 
@@ -59,6 +54,7 @@ public class PlayerWeaponCollider : MonoBehaviour
             {
                 bow = true;
                 sword = false;
+
                 gameState.bow = false;
                 gameState.sword = false;
                 animator.SetBool("bow", false);
@@ -68,17 +64,46 @@ public class PlayerWeaponCollider : MonoBehaviour
                 overlayAnimator.SetBool("bow", false);
                 overlayAnimator.SetBool("sword", false);
 
-
-                animator.SetBool("bow", true);
-                weaponAnimator.SetBool("bow", true);
-                overlayAnimator.SetBool("bow", true);
-
-
-                gameState.bow = true;
-                gameState.sword = false;
-                gameState.changeWeapon = true;
-                weaponSR.enabled = true;
+                start = true;
+                StartCoroutine(SetBow());
             }
+        }
+    }
+
+
+    IEnumerator SetBow()
+    {
+        while (start)
+        {
+            yield return new WaitForSeconds(0.5f);
+            animator.SetBool("bow", true);
+            weaponAnimator.SetBool("bow", true);
+            overlayAnimator.SetBool("bow", true);
+
+            gameState.bow = true;
+            gameState.sword = false;
+            gameState.changeWeapon = true;
+            weaponSR.enabled = true;
+
+            start = false;
+        }
+    }
+
+    IEnumerator SetSword()
+    {
+        while (start)
+        {
+            yield return new WaitForSeconds(0.5f);
+            animator.SetBool("sword", true);
+            weaponAnimator.SetBool("sword", true);
+            overlayAnimator.SetBool("sword", true);
+
+            gameState.bow = false;
+            gameState.sword = true;
+            gameState.changeWeapon = true;
+            weaponSR.enabled = true;
+
+            start = false;
         }
     }
 }
